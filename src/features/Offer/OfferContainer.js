f6564a24deb9ffc528a21511189bc517fc6ffbd9
khanumhuseynova <khanimhuseynovaa@gmail.com>
2022-03-14 18:56:11 +0400

    edited

--- src/features/Offer/OfferContainer.js
+++ src/features/Offer/OfferContainer.js
@@ -9,13 +9,10 @@ const columns = [
   { field: 'title', headerName: 'Title',  headerAlign: 'center', width: 300 },
   { field: 'description', headerName: 'Description',  headerAlign: 'center', width: 350},
   { field: 'img', headerName: 'Image',  headerAlign: 'center', width: 300},
-  { field: 'button', headerName: 'Delete', headerAlign: 'center'},
+ <DeleteOutlineIcon/>
  
 ]
-const tableIcons = {
-  
-  Delete: () => <DeleteOutlineIcon />,
-} ;
+
 
 const OfferContainer = () => {
 
@@ -30,6 +27,10 @@ const OfferContainer = () => {
 
   console.log(tableData)
 
+  
+  
+  
+  
   return (
     <div style={{ maxHeight: 450, width: '1235px' }}>
          <h1 className="names">Offers
@@ -38,22 +39,9 @@ const OfferContainer = () => {
           
           </h1>
       <DataGrid className="data"
-      
         rows={tableData}
         columns={columns}
         pageSize={12}
-        icons={tableIcons}
-        editable={{
-          onRowDelete: selectedRow => new Promise((resolve, _reject) => {
-            const index = selectedRow.tableDatas.id;
-            const updatedRows = [...tableData]
-            updatedRows.splice(index, 1)
-            setTimeout(() => {
-              setTableData(updatedRows)
-              resolve()
-            }, 2000)
-          })
-        }}
       />
     </div>
   )
